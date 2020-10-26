bd5a78847f559816281056323eba447ef99d6044
Tyler Gasperlin <gasperlint@gmail.com>
2020-10-25 19:43:27 -0700

    Add breadth first search function

--- src/Trees/BinarySearchTree.js
+++ src/Trees/BinarySearchTree.js
@@ -177,6 +177,26 @@ class BinarySearchTree {
             }
         }
     }
+
+    breadthFirstSearch() {
+        let currentNode = this.root;
+        let list = [];
+        let queue = []; // queue is wht causes bfs to use more memory -- wide tree = large queue
+        queue.push(currentNode);
+        while (queue.length > 0) {
+            currentNode = queue.shift();
+            list.push(currentNode.value);
+            if (currentNode.left) {
+                queue.push(currentNode.left)
+            }
+            if (currentNode.right) {
+                queue.push(currentNode.right)
+            }
+        }
+
+        return list;
+
+    }
 }
 
 //      9
